a64e18b8084680b78e72b8be71429edbd9bede32
fdmesquita <fannydemesquita@gmail.com>
2021-09-25 17:34:41 +0200

    delete h1

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -1,16 +1,12 @@
 /* animation title */
 
+// const animMainTitle = document.querySelector("h1");
 
-const animMainTitle = document.querySelector("h1");
+// let typeHOne = new Typewriter(animMainTitle, {
+//     delay: 100,
+//     cursor: ""
+//   })
 
-let typeHOne = new Typewriter(animMainTitle, {
-    delay: 100,
-    cursor: ""
-  })
-  
-  typeHOne
-  .typeString("L'Odyssey, Qui sommes nous?")
-  .start()
-
-
-
+//   typeHOne
+//   .typeString("L'Odyssey, Qui sommes nous?")
+//   .start()
